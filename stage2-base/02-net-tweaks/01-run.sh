2ee2ebc4c65161fb9d027a76a105f305aa382b0e
Maciej Małecki <me@mmalecki.com>
2022-11-16 14:19:01 +0100

    Migrate to systemd networking and DNS resolution

--- stage2-base/02-net-tweaks/01-run.sh
+++ stage2-base/02-net-tweaks/01-run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -e
 
-install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
-install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
-
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_netconf 1
-EOF
+install -v -d						"${ROOTFS_DIR}/etc/wpa_supplicant"
+install -v -m 600 files/wpa_supplicant-wlan0.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
+install -v -m 644 files/wlan0.network			"${ROOTFS_DIR}/etc/systemd/network/"
+install -v -m 644 files/eth0.network			"${ROOTFS_DIR}/etc/systemd/network/"
 
 if [ -v WPA_COUNTRY ]; then
 	on_chroot <<- EOF
@@ -17,10 +14,10 @@ fi
 if [ -v WPA_ESSID ] && [ -v WPA_PASSWORD ]; then
 on_chroot <<EOF
 set -o pipefail
-wpa_passphrase "${WPA_ESSID}" "${WPA_PASSWORD}" | tee -a "/etc/wpa_supplicant/wpa_supplicant.conf"
+wpa_passphrase "${WPA_ESSID}" "${WPA_PASSWORD}" | tee -a "/etc/wpa_supplicant/wpa_supplicant-wlan0.conf"
 EOF
 elif [ -v WPA_ESSID ]; then
-cat >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf" << EOL
+cat >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant-wlan0.conf" << EOL
 
 network={
 	ssid="${WPA_ESSID}"
@@ -38,3 +35,9 @@ else
     echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
     echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
 fi
+
+on_chroot << EOF
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+systemctl enable wpa_supplicant@wlan0
+EOF

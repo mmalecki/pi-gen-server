a2be51e3386126168f74ca45a1d146c81a0b016d
Maciej Małecki <me@mmalecki.com>
2024-09-23 15:05:01 +0200

    Set up a `resolv.conf`

--- export-image/03-network/01-run.sh
+++ export-image/03-network/01-run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+# This is so that we have a well-known resolver available
+# within the Packer environment, which doesn't have access
+# to 127.0.0.53:53.
+# Later removed by Packer as the final packaging step.
+install -v -m 644 files/resolv.conf "${ROOTFS_DIR}/etc/resolv.conf"
+
 on_chroot <<-EOF
 systemctl enable systemd-resolved
 EOF
